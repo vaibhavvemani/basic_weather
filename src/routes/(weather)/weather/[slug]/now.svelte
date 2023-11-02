accdcb030b6b8c2adf8406537aabb0a0e4cd1e6f
Vaibhav Vemani <vaibhavvemani@gmail.com>
2023-11-02 23:17:54 +0530

    UI: Created a weather card

--- src/routes/(weather)/weather/[slug]/now.svelte
+++ src/routes/(weather)/weather/[slug]/now.svelte
@@ -1,55 +1,136 @@
+<script>
+  import { onMount } from "svelte";
+  import { page } from "$app/stores";
+
+  let location, temperature, condition, condition_icon;
+  let slug = $page.params.slug;
+
+  onMount(async () => {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=58c018eea6c840be83c75506232204&q=${slug}`
+    );
+    const data = await response.json();
+    location = data.location.name;
+    temperature = data.current.temp_f;
+    condition = data.current.condition.text;
+    condition_icon = data.current.condition.icon;
+  });
+</script>
+
 <div class="presentcontainer glass">
-	<div class="title-icon">
-		<h1>Right Now</h1>
-		<!-- <img src={lookin} alt="Condition" class="condition-icon" /> -->
-	</div>
-	<div class="presentinfo">
-		<p>Local Time:</p>
-		<p>Temperature: F/C</p>
-		<p>Feels like: F/C</p>
-		<p>Condition:</p>
-		<p>Wind Speeds: mph/kph</p>
-		<p>Wind gusts:</p>
-		<p>Pressure: mb/in</p>
-		<p>Precipitaion: mm/in</p>
-		<p>Humidity:</p>
-		<p>UV levels:</p>
-	</div>
+  <h1>{location}</h1>
+  <h4>12:34:49AM</h4>
+  <div class="weather-card">
+    <div class="icon-container">
+      <div class="icon">
+        <img src={condition_icon} alt="Weather condition" />
+      </div>
+
+      <div class="temperature">
+        <h1>{temperature}°</h1>
+        <p>Feels like: 89°</p>
+      </div>
+    </div>
+
+    <div class="card-info">
+      <p>Condition: {condition}</p>
+      <hr />
+      <p>Wind Speeds: who cares</p>
+      <hr />
+      <p>Condition: Pretty bad ig</p>
+      <hr />
+    </div>
+  </div>
+  <hr />
+  <div class="presentinfo">
+    <p>Name:</p>
+    <p>Local Time:</p>
+    <p>Temperature: F/C</p>
+    <p>Feels like: F/C</p>
+    <p>Condition:</p>
+    <p>Visibility:</p>
+    <p>Wind Speeds: mph/kph</p>
+    <p>Wind gusts:</p>
+    <p>Pressure: mb/in</p>
+    <p>Precipitaion: mm/in</p>
+    <p>Humidity:</p>
+    <p>UV levels:</p>
+  </div>
 </div>
 
 <style>
-	@import url('https://fonts.googleapis.com/css2?family=Red+Hat+Mono:wght@300;400;500&display=swap');
-	* {
-		font-family: 'Red Hat Mono', monospace;
-		color: white;
-	}
-	* p {
-		font-weight: 500;
-	}
-	.glass {
-		background: linear-gradient(135deg, rgba(226, 215, 215, 0.1), rgba(255, 255, 255, 0));
-		backdrop-filter: blur(10px);
-		-webkit-backdrop-filter: blur(10px);
-		border-radius: 20px;
-		border: 1px solid rgba(255, 255, 255, 0.18);
-		box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.37);
-	}
-	.presentcontainer {
-		/* background-color: #c8c8c847;
+  @import url("https://fonts.googleapis.com/css2?family=Red+Hat+Mono:wght@300;400;500&display=swap");
+  * {
+    font-family: "Red Hat Mono", monospace;
+    color: white;
+  }
+  * p {
+    font-weight: 500;
+  }
+  .glass {
+    background: linear-gradient(
+      135deg,
+      rgba(226, 215, 215, 0.1),
+      rgba(255, 255, 255, 0)
+    );
+    backdrop-filter: blur(10px);
+    -webkit-backdrop-filter: blur(10px);
+    border-radius: 20px;
+    border: 1px solid rgba(255, 255, 255, 0.18);
+    box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.37);
+  }
+  .presentcontainer {
+    /* background-color: #c8c8c847;
 		border: 1px solid white;
 		box-shadow: inset 10px 10px 20px 20px #a8a8a8a1, inset -10px -10px 40px 50px #a3a3a3a2;
 		backdrop-filter: blur(4px); */
-		border-radius: 10px;
-		height: 100%;
-		width: 100%;
-		padding: 10px;
-	}
-	.title-icon {
-		display: flex;
-		align-items: center;
-	}
+    border-radius: 10px;
+    height: 100%;
+    width: 100%;
+    padding: 10px;
+  }
+  .presentinfo {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
 
-	.title-icon h1 {
-		color: white;
-	}
+    padding-top: 20px;
+  }
+  hr {
+    background-color: gray;
+    height: 2px;
+  }
+  .weather-card {
+    height: 15rem;
+
+    display: flex;
+    gap: 2rem;
+  }
+  .icon-container {
+    width: 50%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .icon {
+    height: 40%;
+    width: 40%;
+  }
+  .icon img {
+    width: 100%;
+    height: 100%;
+
+    object-fit: fill;
+  }
+
+  .card-info {
+    width: 50%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: left;
+    gap: 10px;
+  }
 </style>
